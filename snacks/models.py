ac31a08abfd82df25af4096363a9e26b8370e892
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-03-20 10:42:27 -0400

    beginning of app

---
+++ snacks/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Rabbit(models.Model):
+    name = models.CharField(max_length=200)
+    foods = models.ManyToManyField(to='Food', related_name='foods')
+    owner = models.ForeignKey(
+        to='User', on_delete=models.CASCADE, related_name='rabbits')
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+# add extraneous comment
+# adding more stuff
